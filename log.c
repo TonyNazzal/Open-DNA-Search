55b555e5b957e7bc84a828209d5e59259563d078
Tony E. Nazzal <tony.e.nazzal@gmail.com>
2016-03-03 09:03:39 -0800

    add timestamp to logs

--- log.c
+++ log.c
@@ -18,7 +18,7 @@
 
    How to contact the author by electronic mail: tony.e.nazzal@gmail.com
 
-*/
+ */
 
 #include "log.h"
 
@@ -36,6 +36,9 @@ void l(string line, logfile *mylogfile)
 {
 	char buf[strlen(line) + 255];
 	strcpy(buf,"");
+	time_t t;
+	time(&t);
+	ctime_r(&t, buf);
 	buf[strlen(buf)-1] = '\0';
 	strcat(buf, " : ");
 	strcat(buf, line);
@@ -44,14 +47,16 @@ void l(string line, logfile *mylogfile)
 	fputs(buf, mylogfile->pointer);
 	fflush(mylogfile->pointer);
 	pthread_mutex_unlock(&mylogfile->lock);
-
 };
 
 void lf(string line)
 {
-    //logfile *mylogfile;
+	//logfile *mylogfile;
 	char buf[strlen(line) + 255];
 	strcpy(buf,"");
+	time_t t;
+	time(&t);
+	ctime_r(&t, buf);
 	buf[strlen(buf)-1] = '\0';
 	strcat(buf, " : ");
 	strcat(buf, line);
@@ -60,38 +65,38 @@ void lf(string line)
 	fputs(buf, mylogfile->pointer);
 	fflush(mylogfile->pointer);
 	pthread_mutex_unlock(&mylogfile->lock);
-    free(line);
+	free(line);
 };
 
 void ls(const string fmt, ...)
 {
-    int n, size = 40;
-    string p;
-    string np;
-    va_list ap;
-    p = malloc(size);
-    if(NULL == p)
-        return lf("logging error");
+	int n, size = 40;
+	string p;
+	string np;
+	va_list ap;
+	p = malloc(size);
+	if(NULL == p)
+		return lf("logging error");
 
-    while (TRUE)
-    {
+	while (TRUE)
+	{
 
-        va_start(ap, fmt);
-        n = vsnprintf(p, size, fmt, ap);
-        va_end(ap);
+		va_start(ap, fmt);
+		n = vsnprintf(p, size, fmt, ap);
+		va_end(ap);
 
-        if (n > -1 && n < size)
-           break;
-        if (n > -1)
-           size = n+1;
-        else
-           size *= 2;
-        if (NULL == (np = realloc(p, size))) {
-           free(p);
-           return lf("logging error");
-        } else {
-           p = np;
-        }
-    }
-    lf(p);
+		if (n > -1 && n < size)
+			break;
+		if (n > -1)
+			size = n+1;
+		else
+			size *= 2;
+		if (NULL == (np = realloc(p, size))) {
+			free(p);
+			return lf("logging error");
+		} else {
+			p = np;
+		}
+	}
+	lf(p);
 }
